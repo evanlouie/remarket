ff2b88b52c8e422d3a81d3f379979aa4308f7de9
Ryan Booth <ryan@ryanabooth.com>
2013-03-17 22:00:25 -0700

    Added a contact form. Needs to be changed when we have this up on a server.

--- application/views/partials/footer.blade.php
+++ application/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar navbar-bottom">
   <div class="navbar-inner" style="background: #4c4c4c">
     <form class="navbar-form pull-right" action="">
-      <button class="btn btn-link">Button</button>
+      <a href="/contact" class="btn btn-link">Contact</a>
       <button class="btn btn-link">Button</button>
       <button class="btn btn-link">Button</button>
     </form>
