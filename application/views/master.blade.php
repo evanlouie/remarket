7a79c0e20dc32938a11c631d441f54597d2faaad
Evan Louie <evan@evanlouie.com>
2013-03-18 12:54:24 -0700

    added warning to master template

--- application/views/master.blade.php
+++ application/views/master.blade.php
@@ -5,6 +5,9 @@
 		<div id="wrapper">
 			<div class='content'>
 				@yield('loginbar')
+				@if(Session::has('alert'))
+					{{ Session::get('alert') }}
+				@endif
 				@yield('content')
 			</div>
 			@yield('footer')
