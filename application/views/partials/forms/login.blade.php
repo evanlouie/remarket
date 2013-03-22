652f1b82591d22e8ae539ef20620efd4c871753c
Anthony Chow <anthony0012@gmail.com>
2013-03-22 02:26:13 -0700

    tweaked header

--- application/views/partials/forms/login.blade.php
+++ application/views/partials/forms/login.blade.php
@@ -2,9 +2,24 @@
 <div class='pull-left'>
 	<a href="/" class="btn btn-link" style="text-shadow:none; font-size: 20px; color: #ffffff;">Reduce, reuse, recycle, REMARKET</a>
 </div>
+
 <div class='pull-right' style="margin-right: 10px;">
-	<a class="btn btn-primary" href="/account/myListings">My Account</a>
-	<a class="btn btn-danger" href="/account/logout/">Logout</a>
+	<form class="navbar-form pull-left">
+		<input type="text" class="span5">
+		<button type="submit" class="btn btn-primary">Search</button>
+		<a href="/listing/" class="btn btn-success" style="margin-right:4px">Browse</a>
+	</form>
+	<!-- <a class="btn btn-primary" href="/account/myListings">My Account</a> -->
+	<!-- <a class="btn btn-danger" href="/account/logout/">Logout</a> -->
+
+	<a class="btn dropdown-toggle btn-info" data-toggle="dropdown" href="#">
+		My Account
+		<span class="caret"></span>
+	</a>
+	<ul class="dropdown-menu">
+		<a href="/home/">test</a>
+	</ul>
+
 </div>
 @else
 <div class='pull-left'>
@@ -19,9 +34,9 @@
 @endif
 
 <script>
-	function login() {
-		e = $('#email').val();
-		p = $('#password').val();
+function login() {
+	e = $('#email').val();
+	p = $('#password').val();
 		// alert(e);/
 		path = window.location.pathname;
 		$.post("/account/login/", {email: e, password: p}).done(function() {
@@ -33,4 +48,4 @@
 		return false;
 	});
 
-</script>
+	</script>
